e000149a182b6647fa26d05f2aadf5783a9a96be
Nick Bair <nick.bair@cloud-elements.com>
2020-11-16 10:14:47 -0700

    Check if template Ids are valid, clean up error messaging.

--- DeleteFormulaInstances/env.js
+++ DeleteFormulaInstances/env.js
@@ -6,28 +6,28 @@ const checkEnv = () => {
 
   for (const key in env) {
     if (env.hasOwnProperty(key) && isInvalid(env[key])) {
-      errors.push(`ERROR: expected value for ${key} in .env, got null or undefined.`);
+      errors.push(`ERROR: Expected value for ${key} in .env, got null or undefined.`);
     }
   }
 
   if (doesNotInclude(['STAGING', 'US_PROD', 'EU_PROD'], env.ceEnv)) {
-    errors.push(`ERROR: expected value for CE_ENV in .env to be one of the following: 'STAGING', 'US_PROD', 'EU_PROD', but found ${env.ceEnv}.`);
+    errors.push(`ERROR: Expected value for CE_ENV in .env to be one of the following: 'STAGING', 'US_PROD', 'EU_PROD', but found ${env.ceEnv}.`);
   }
 
   if (doesNotInclude(['GET', 'DELETE'], env.mode)) {
-    errors.push(`ERROR: expected value for MODE in .env to be one of the following: 'GET', 'CREATE', 'DELETE', but found ${env.mode}.`);
+    errors.push(`ERROR: Expected value for MODE in .env to be one of the following: 'GET', 'DELETE', but found ${env.mode}.`);
   }
 
   env.templateIds.forEach(id => {
     if (isNaN(id)) {
-      errors.push(`ERROR: expected value for formula template ID in .env to be integer, but found ${id}.`);
+      errors.push(`ERROR: Expected value for formula template ID in .env to be integer, but found ${id}.`);
     }
   });
 
   if (errors.length > 0) {
-    console.error('ERROR: found the following errors while checking the environment variables required for running this script.');
+    console.error('ERROR: Found the following errors while checking the environment variables required for running this script.');
     errors.forEach(e => console.error(e));
-    process.exit(1);
+    process.exit();
   }
 };
 
--- DeleteFormulaInstances/index.js
+++ DeleteFormulaInstances/index.js
@@ -19,7 +19,7 @@ const getInstances = async (templateId, token) => {
     const res = await axios.get(url, { headers: headers(userSecret, orgSecret), timeout: 5000 });
     return res;
   } catch (e) {
-    console.error(e);
+    console.error(`Error: ${e.response.data.message}, requestId: ${e.response.data.requestId}`);
   }
 };
 
@@ -43,10 +43,28 @@ const getPagedInstances = async (templateId) => {
   return { templateId, results: results.flat() };
 };
 
-const getAllInstances = async () => {
-  const { templateIds } = getEnv();
+const getAllInstances = async (templateIds) => {
   return Promise.all(templateIds.map(t => getPagedInstances(t)));
-}
+};
+
+const checkTemplateIds = async () => {
+  const { templateIds } = getEnv();
+  return (await Promise.all(templateIds.map(id => checkTemplateId(id)))).filter(t => t);
+};
+
+const checkTemplateId = async (templateId) => {
+  const { ceEnv, userSecret, orgSecret } = getEnv();
+
+  try {
+    const res = await axios.get(`https://${baseUrl(ceEnv)}/elements/api-v2/formulas/${templateId}`, {
+      headers: headers(userSecret, orgSecret),
+      timeout: 5000
+    });
+    return res.data.id;
+  } catch (e) {
+    console.error(`Error: ${e.response.data.message}, requestId: ${e.response.data.requestId}`);
+  }
+};
 
 const deleteInstances = async (templateId, instanceIds) => instanceIds.map(i => deleteInstance(templateId, i));
 
@@ -55,7 +73,8 @@ const deleteInstance = async (templateId, instanceId) => {
 
   try {
     await axios.delete(`https://${baseUrl(ceEnv)}/elements/api-v2/formulas/${templateId}/instances/${instanceId}`, {
-      headers: headers(userSecret, orgSecret)
+      headers: headers(userSecret, orgSecret),
+      timeout: 5000
     });
   } catch (e) {
     console.error(`Error: ${e.response.data.message}, requestId: ${e.response.data.requestId}`);
@@ -69,9 +88,12 @@ const deleteAllInstances = async (templates) => Promise.all(templates.map(async 
 
 const start = async () => {
   const { mode, ceEnv } = getEnv();
-  console.log(`INFO: running script in ${mode} mode for Cloud Elements ${ceEnv}.`);
+  console.log(`INFO: Running script in ${mode} mode for Cloud Elements ${ceEnv}.`);
   
-  const instances = await getAllInstances();
+  const templateIds = await checkTemplateIds();
+  console.log(`INFO: Found ${templateIds.length} valid template ID(s), skipping any invalid template ID(s).`);
+
+  const instances = await getAllInstances(templateIds);
 
   instances.forEach(i => {
     console.log(`INFO: Formula template ID ${i.templateId} found ${i.results.length} instance(s).`);
